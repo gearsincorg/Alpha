f8e4ced35ed3fa5cf688f44c256975e5c9a20671
Phil Malone <phil.malone@mr-phil.com>
2026-05-31 18:44:28 -0400

    Updated IMU test

--- Alpha IMU/src/main/java/first/robot/subsystems/IMUsubsystem.java
+++ Alpha IMU/src/main/java/first/robot/subsystems/IMUsubsystem.java
@@ -11,19 +11,34 @@ import org.wpilib.smartdashboard.SmartDashboard;
 
 public class IMUsubsystem extends SubsystemBase {
 
-  OnboardIMU obIMU = new OnboardIMU(MountOrientation.FLAT);
+  OnboardIMU obIMUF = new OnboardIMU(MountOrientation.FLAT);
+  OnboardIMU obIMUP = new OnboardIMU(MountOrientation.PORTRAIT);
+  OnboardIMU obIMUL = new OnboardIMU(MountOrientation.LANDSCAPE);
 
   /** Creates a new IMU. */
   public IMUsubsystem() {
-    obIMU.resetYaw();
+    obIMUF.resetYaw();
+    obIMUL.resetYaw();
+    obIMUP.resetYaw();
   }
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("pitch", Math.toDegrees(obIMU.getAngleY()));
-    SmartDashboard.putNumber("roll",  Math.toDegrees(obIMU.getAngleX()));
-    SmartDashboard.putNumber("yaw",   Math.toDegrees(obIMU.getAngleZ()));
-    SmartDashboard.putNumber("yaw2",  Math.toDegrees(obIMU.getYawRadians()));
+    SmartDashboard.putNumber("F Angle X",  Math.toDegrees(obIMUF.getAngleX()));
+    SmartDashboard.putNumber("F Angle Y", Math.toDegrees(obIMUF.getAngleY()));
+    SmartDashboard.putNumber("F Angle Z",   Math.toDegrees(obIMUF.getAngleZ()));
+    SmartDashboard.putNumber("F Yaw",  Math.toDegrees(obIMUF.getYawRadians()));
+
+    SmartDashboard.putNumber("L Angle X",  Math.toDegrees(obIMUL.getAngleX()));
+    SmartDashboard.putNumber("L Angle Y", Math.toDegrees(obIMUL.getAngleY()));
+    SmartDashboard.putNumber("L Angle Z",   Math.toDegrees(obIMUL.getAngleZ()));
+    SmartDashboard.putNumber("L Yaw",  Math.toDegrees(obIMUL.getYawRadians()));
+
+    SmartDashboard.putNumber("P Angle X",  Math.toDegrees(obIMUP.getAngleX()));
+    SmartDashboard.putNumber("P Angle Y", Math.toDegrees(obIMUP.getAngleY()));
+    SmartDashboard.putNumber("P Angle Z",   Math.toDegrees(obIMUP.getAngleZ()));
+    SmartDashboard.putNumber("P Yaw",  Math.toDegrees(obIMUP.getYawRadians()));
   }
 }
